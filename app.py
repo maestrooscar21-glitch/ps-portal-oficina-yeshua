37d654cfc64d41930c01e1ce44f8186cbf020d7f
maestrooscar21-glitch <maestrooscar21@gmail.com>
2026-09-12 10:55:04 -0300

    Update app.py1.5.0

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from datetime import date, datetime, timedelta
-from zoneinfo import ZoneiInfo
+from zoneinfo import ZoneInfo
 from supabase import create_client
 
 st.set_page_config(
